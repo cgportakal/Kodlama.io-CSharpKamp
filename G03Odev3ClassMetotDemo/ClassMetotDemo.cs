e1ce02c10f9b7632d588869bf55da48bd88f9164
Cafer Gökmen Portakal <cgportakal@outlook.com>
2021-01-19 21:00:48 +0300

    Class Metot Demo

---
+++ G03Odev3ClassMetotDemo/ClassMetotDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace G03Odev3ClassMetotDemo
+{
+    class ClassMetotDemo
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
