f58e03024c68a3324bfba27aa5c235342cb2c73e
Cafer Gökmen Portakal <cgportakal@outlook.com>
2021-01-20 23:59:39 +0300

    Generics geliştirildi. MyList Geliştirildi.

--- G04E03GenericsIntro/GenericsIntro.cs
+++ G04E03GenericsIntro/GenericsIntro.cs
@@ -9,6 +9,13 @@ namespace G04Eg03GenericsIntro
             MyList<string> isimler = new MyList<string>();
             //MyList<int> idler = new MyList<int>();
             isimler.Add("Cafer");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Gökmen");
+            Console.WriteLine(isimler.Length);
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
         }
     }
 }
